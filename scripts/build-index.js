f0ff1e4489bfdf122f9189313e63889cb12f71c6
Petko D. Petkov <pdp@gnucitizen.org>
2019-01-23 11:45:01 +0000

    Lazy loading of transforms for quicker CMD access.

---
+++ scripts/build-index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const root = path.join(__dirname, '..', 'lib', 'transforms')
+
+const code = fs.readdirSync(root)
+    .filter((name) => {
+        return name !== 'index.js'
+    })
+    .map((name) => {
+        return { name, module: require(path.join(root, name)) }
+    })
+    .map(({ name, module }) => {
+        return Object.entries(module)
+            .map(([transformer, module]) => {
+                const { alias, title, description, types, options } = module
+
+                return `exports[${JSON.stringify(transformer)}] = ${JSON.stringify({alias, title, description, types, options}, '', '    ')}
+
+exports[${JSON.stringify(transformer)}].load = function () { return require(${JSON.stringify('.' + path.sep + name)})[${JSON.stringify(transformer)}] }`
+            })
+            .join('\n\n')
+    })
+    .join('\n\n')
+
+fs.writeFileSync(path.join(root, 'index.js'), code + '\n')
