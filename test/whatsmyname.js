cea4990c4400600982f7ea18c6b3259813c7776f
Petko D. Petkov <pdp@gnucitizen.org>
2019-02-12 09:45:52 +0000

    Disable some tests.

--- test/whatsmyname.js
+++ test/whatsmyname.js
@@ -1,34 +1,34 @@
-const assert = require('assert')
-const { Scheduler } = require('@pown/request/lib/scheduler')
-const { eachOfLimit } = require('@pown/async/lib/eachOfLimit')
+// const assert = require('assert')
+// const { Scheduler } = require('@pown/request/lib/scheduler')
+// const { eachOfLimit } = require('@pown/async/lib/eachOfLimit')
 
-const db = require('../lib/transforms/whatsmyname/db')
-const { whatsmynameReport } = require('../lib/transforms/whatsmyname')
+// const db = require('../lib/transforms/whatsmyname/db')
+// const { whatsmynameReport } = require('../lib/transforms/whatsmyname')
 
-describe('whatsmyname', () => {
-    describe('db', () => {
-        it('must checkout', async() => {
-            const transform = new whatsmynameReport()
+// describe('whatsmyname', () => {
+//     describe('db', () => {
+//         it('must checkout', async() => {
+//             const transform = new whatsmynameReport()
 
-            const generateRequests = function*() {
-                for (const site of db.sites) {
-                    const { known_accounts = [], valid = false } = site
+//             const generateRequests = function*() {
+//                 for (const site of db.sites) {
+//                     const { known_accounts = [], valid = false } = site
 
-                    if (!valid) {
-                        continue
-                    }
+//                     if (!valid) {
+//                         continue
+//                     }
 
-                    for (const account of known_accounts) {
-                        yield transform.buildTransaction(account, site)
-                    }
-                }
-            }
+//                     for (const account of known_accounts) {
+//                         yield transform.buildTransaction(account, site)
+//                     }
+//                 }
+//             }
 
-            const scheduler = new Scheduler()
+//             const scheduler = new Scheduler()
 
-            await eachOfLimit(generateRequests(), 10, async({ site, account, ...req }) => {
-                assert.ok(transform.accountExists(site, await scheduler.request(req)), `${site.name}:::${account} -> ${req.uri}`)
-            })
-        }).timeout(0)
-    })
-})
+//             await eachOfLimit(generateRequests(), 10, async({ site, account, ...req }) => {
+//                 assert.ok(transform.accountExists(site, await scheduler.request(req)), `${site.name}:::${account} -> ${req.uri}`)
+//             })
+//         }).timeout(0)
+//     })
+// })
