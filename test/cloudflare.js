47d934431df111760c075b29956ef5758152682a
Petko D. Petkov <pdp@gnucitizen.org>
2019-03-21 06:06:17 +0000

    Improved tests.

--- test/cloudflare.js
+++ test/cloudflare.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-const { makeStringNode } = require('./helpers')
+const { makeNode } = require('../lib/utils')
 const cloudflare = require('../lib/transforms/cloudflare')
 
 describe('cloudflare', () => {
@@ -8,18 +8,18 @@ describe('cloudflare', () => {
         const t = new cloudflare.cloudflareDnsQuery()
 
         it('must transform A records', async function() {
-            const results = await t.run([makeStringNode('secapps.com')], { type: 'A' })
+            const results = await t.run([makeNode({ type: 'string', label: 'secapps.com' })], { type: 'A' })
 
             assert.ok(results.length > 0, 'there are results')
             assert.ok(results.every(({ type }) => type === 'ipv4'), 'all results are of type ipv4')
-        })
+        }).timeout(30000)
 
         it('must transform AAAA records', async function() {
-            const results = await t.run([makeStringNode('secapps.com')], { type: 'AAAA' })
+            const results = await t.run([makeNode({ type: 'string', label: 'secapps.com' })], { type: 'AAAA' })
 
             assert.ok(results.length > 0, 'there are results')
             assert.ok(results.every(({ type }) => type === 'ipv6'), 'all results are of type ipv6')
-        })
+        }).timeout(30000)
 
         // TODO: add more tests
     })
---
+++ test/recon.js
@@ -0,0 +1,37 @@
+const assert = require('assert')
+
+const { Recon } = require('../lib/recon')
+const { makeNode } = require('../lib/utils')
+
+describe('recon', () => {
+    describe('#addNodes', () => {
+        it('must add node', async function() {
+            const r = new Recon()
+
+            r.addNodes([makeNode({ type: 'string', label: 'test' })])
+
+            r.select('*')
+
+            assert.ok(r.selection.length === 1)
+            assert.ok(r.selection[0].data('props').string === undefined)
+        })
+
+        it('must update node if node exists', async function() {
+            const r = new Recon()
+
+            r.addNodes([makeNode({ type: 'string', label: 'test' })])
+
+            r.select('*')
+
+            assert.ok(r.selection.length === 1)
+            assert.ok(r.selection[0].data('props').string === undefined)
+
+            r.addNodes([makeNode({ type: 'string', label: 'test', props: { string: 'test' } })])
+
+            r.select('*')
+
+            assert.ok(r.selection.length === 1)
+            assert.ok(r.selection[0].data('props').string === 'test')
+        })
+    })
+})
