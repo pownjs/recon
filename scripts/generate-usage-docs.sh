d6e1c3a4c58f68ea46ad8c374edd237381b0b98c
Petko D. Petkov <pdp@gnucitizen.org>
2019-06-24 19:36:50 +0000

    Added summary.

--- scripts/generate-usage-docs.sh
+++ scripts/generate-usage-docs.sh
@@ -2,7 +2,7 @@
 
 COMMANDS="pown"
 SUBCOMMANDS="recon"
-SUBSUBCOMMANDS="transform select traverse add remove merge diff group ungroup load save import export remote"
+SUBSUBCOMMANDS="transform select traverse add remove merge diff group ungroup load save import export remote summary"
 
 export POWN_ROOT=.
 
