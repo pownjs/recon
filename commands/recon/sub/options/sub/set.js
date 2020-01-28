57fdd245c29662f4fdbfc4f1eaa7232aeb759fd8
Petko D. Petkov <pdp@gnucitizen.org>
2020-01-28 18:29:20 +0000

    Optionally support json with boolean flag.

--- commands/recon/sub/options/sub/set.js
+++ commands/recon/sub/options/sub/set.js
@@ -6,16 +6,29 @@ exports.yargs = {
     builder: (yargs) => {
         yargs.option('category', {
             alias: ['c'],
+            type: 'string',
             describe: 'Select category',
             default: 'global'
+        })
+
+        yargs.option('json', {
+            alias: ['j'],
+            type: 'boolean',
+            describe: 'Assume the option is a json string',
+            default: false
         })
     },
 
     handler: (argv) => {
         const { options } = require('../../../lib/globals/options')
 
-        const { category, name, value } = argv
+        const { category, json, name, value } = argv
 
-        options.setOption(category, name, JSON.parse(value))
+        if (json) {
+            options.setOption(category, name, JSON.parse(value))
+        }
+        else {
+            options.setOption(category, name, value)
+        }
     }
 }
