8ae0edf0435e841c0dabd96d593202f660028742
Petko D. Petkov <pdp@gnucitizen.org>
2020-03-13 14:53:02 +0000

    Added bar.

--- commands/recon/lib/globals/recon.js
+++ commands/recon/lib/globals/recon.js
@@ -1,3 +1,5 @@
+const { Bar } = require('@pown/cli/lib/bar')
+
 const { Recon } = require('../../../../lib/recon')
 
 const recon = new Recon()
@@ -6,5 +8,41 @@ recon.on('info', console.info.bind(console))
 recon.on('warn', console.warn.bind(console))
 recon.on('error', console.error.bind(console))
 recon.on('debug', console.debug.bind(console))
+
+const bars = {}
+
+recon.on('barStart', (name, { total = 0 }) => {
+    if (total < 1000) {
+        return
+    }
+
+    const bar = new Bar()
+
+    bar.start(total, 0)
+
+    bars[name] = bar
+})
+
+recon.on('barStep', (name, { step = 0 }) => {
+    const bar = bars[name]
+
+    if (!bar) {
+        return
+    }
+
+    bar.update(step)
+})
+
+recon.on('barEnd', (name) => {
+    const bar = bars[name]
+
+    if (!bar) {
+        return
+    }
+
+    bar.stop()
+
+    delete bars[name]
+})
 
 module.exports = { recon }
