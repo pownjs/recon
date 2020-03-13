2880a2acdfa03fd857c5a6a4c1908d44e0c2c157
Petko D. Petkov <pdp@gnucitizen.org>
2020-03-13 11:07:39 +0000

    Added some basic tests.

---
+++ test/detect.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+
+const { isSubdomain } = require('../lib/detect')
+
+describe('detect', () => {
+    describe('#isSubdomain', () => {
+        assert.ok(!isSubdomain('acme.com'), 'acme.com is not subodmain')
+        assert.ok(!!isSubdomain('sub.acme.com'), 'sub.acme.com is subodmain')
+    })
+})
