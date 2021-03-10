e1da98f8b6b900934df4ea02e271eadf5822e805
Petko D. Petkov <pdp@websecurify.com>
2021-03-10 08:12:40 +0000

    Added missing aliases.

--- commands/recon/sub/template/index.js
+++ commands/recon/sub/template/index.js
@@ -1,6 +1,7 @@
 exports.yargs = {
     command: 'template <command>',
     describe: 'Recon template commands',
+    aliases: ['p'],
 
     builder: (yargs) => {
         yargs.command(require('./sub/run').yargs)
