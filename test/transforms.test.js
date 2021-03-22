a5c3205e75c10f814141beeabb92102b4dc0917e
Petko D. Petkov <pdp@websecurify.com>
2021-03-22 22:13:27 +0000

    Added more tags and testing.

--- test/transforms.test.js
+++ test/transforms.test.js
@@ -10,6 +10,14 @@ describe('transforms', () => {
         assert.equal(uniqueAliases.length, aliases.length, 'Duplicate aliases')
     })
 
+    it('validates tags', () => {
+        for (let [name, value] of Object.entries(transforms)) {
+            const tags = value.tags || []
+
+            assert.ok(tags.includes('ce'), `Transform ${name} does not contain the ce tag.`)
+        }
+    })
+
     it('validates options', () => {
         const aliases = [].concat(...Object.entries(transforms).map(([name, { options }]) => Object.entries(options || {}).map(({ alias }) => alias || [])))
         const uniqueAliases = Array.from(new Set(aliases))
