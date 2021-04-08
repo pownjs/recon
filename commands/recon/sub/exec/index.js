190d161a1b2363dbc2fb9ceaa08e55211da81ff5
Petko D. Petkov <pdp@websecurify.com>
2021-04-08 20:02:14 +0000

    Added missing exec options.

--- commands/recon/sub/exec/index.js
+++ commands/recon/sub/exec/index.js
@@ -3,7 +3,12 @@ exports.yargs = {
     describe: 'Execute js file',
     aliases: ['c'],
 
-    builder: (yargs) => {},
+    builder: (yargs) => {
+        const { installReadOptions, installWriteOptions } = require('../../lib/handlers/file')
+
+        installReadOptions(yargs)
+        installWriteOptions(yargs)
+    },
 
     handler: async(argv) => {
         const { files } = argv
@@ -13,6 +18,10 @@ exports.yargs = {
 
         const { recon } = require('../../lib/globals/recon')
 
+        const { handleReadOptions, handleWriteOptions } = require('../../lib/handlers/file')
+
+        await handleReadOptions(argv, recon)
+
         for (let file of Array.isArray(files) ? files : [files]) {
             const module = require(path.join(process.cwd(), file)) // TODO: we should not be doing our own path resolve
 
@@ -20,5 +29,7 @@ exports.yargs = {
                 await module(recon)
             }
         }
+
+        await handleWriteOptions(argv, recon)
     }
 }
