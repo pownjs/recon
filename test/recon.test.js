afda50d4f4ead3570d60f53acb39a0abef226274
Petko D. Petkov <pdp@websecurify.com>
2021-06-16 11:35:39 +0000

    Other basic tests.

--- test/recon.test.js
+++ test/recon.test.js
@@ -33,5 +33,23 @@ describe('recon', () => {
             assert.ok(r.selection.length === 1)
             assert.ok(r.selection[0].data('props').string === 'test')
         })
+
+        it('must preserve buffers in data structs', async function() {
+            const r = new Recon()
+
+            const buffer = Buffer.from('buffer')
+
+            await r.addNodes([makeNode({ type: 'buffer', label: 'buffer', props: { buffer } })])
+
+            r.select('*')
+
+            assert.ok(r.selection.length === 1)
+            assert.ok(r.selection[0].data('props').buffer === buffer)
+
+            const d = await r.serialize()
+
+            assert.ok(d.elements.nodes.length === 1)
+            assert.ok(d.elements.nodes[0].data.props.buffer === buffer)
+        })
     })
 })
